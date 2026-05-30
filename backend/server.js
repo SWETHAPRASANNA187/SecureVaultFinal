3913454819e554420160675b4fe658b3a0118c43
Swetha P <pswetha182007@gmail.com>
2026-05-30 20:40:07 +0530

    Updated files

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,5 @@
-
-require("dotenv").config();
-
+import dotenv from "dotenv";
+dotenv.config();
 const express = require("express");
 const cors = require("cors");
 const multer = require("multer");
